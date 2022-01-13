d8d00991782f8d2de142699cbdf5ec26bda16fb2
nguetcheu <dnguetcheu@gmail.com>
2022-01-13 22:03:02 +0100

    creation et remplissage de la logique des modules dans le dossier views

---
+++ views/modules/header.php
@@ -0,0 +1,71 @@
+<header class="main-header">
+
+    <!--========================================
+    =          LOGO DU SITE             =
+    =========================================-->
+    <a href="" class="logo">
+
+    <!-- logo mini -->
+    <span class="logo-mini">
+        <img src="views/img/template/icon-blanc.png" class="img-responsive" style="padding: 10px;">
+    </span>
+
+    <!-- logo normal -->
+    <span class="logo-lg">
+        <img src="views/img/template/logo-blanc-lineal.png" class="img-responsive" style="padding: 10px 0px;">
+    </span>
+
+    </a>
+
+    <!--========================================
+    =          BARRE DE NAVIGATION            =
+    =========================================-->
+    <nav class="navbar navbar-static-top" role="navigation">
+
+        <!-- Bouton de navigation -->
+
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a> 
+
+        <!-- Profil utilisateur -->
+
+		<div class="navbar-custom-menu">
+
+            <ul class="nav navbar-nav">
+
+                <li class="dropdown user user-menu">
+
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        
+                        <img class="user-image" src="views/img/users/default/anonymous.png">
+                                            
+                        <span class="hidden-xs">Administrateur</span>
+
+                    </a>
+
+                    <!-- Menu déroulant -->
+
+                    <ul class="dropdown-menu">
+
+                        <li class="user-body">
+
+                            <div class="pull-right">
+
+                                <a class="btn-default btn-flat" href="logout">Déconnexion</a>
+
+                            </div>
+
+                        </li>
+
+                    </ul>
+
+                </li>
+
+            </ul>
+
+        </div>
+    
+    </nav>
+
+</header>
---
+++ views/modules/menu.php
@@ -0,0 +1,118 @@
+<aside class="main-sidebar">
+
+    <section class="sidebar">
+
+        <ul class="sidebar-menu">
+
+            <li class="active">
+
+                <a href="">
+
+                    <i class="fa fa-home"></i>
+                    <span>Accueil</span>
+
+                </a>
+
+            </li>
+
+            <li class="active">
+
+                <a href="">
+
+                    <i class="fa fa-user"></i>
+                    <span>Utilisateurs</span>
+
+                </a>
+
+            </li>
+
+            <li class="active">
+
+                <a href="">
+
+                    <i class="fa fa-th"></i>
+                    <span>Categorie</span>
+
+                </a>
+
+            </li>
+
+            <li class="active">
+
+                <a href="">
+
+                    <i class="fa fa-product-hunt"></i>
+                    <span>Produits</span>
+
+                </a>
+
+            </li>
+
+            <li class="active">
+
+                <a href="">
+
+                    <i class="fa fa-users"></i>
+                    <span>Clients</span>
+
+                </a>
+
+            </li>
+
+            <li class="treeview">
+
+                <a href="">
+
+                    <i class="fa fa-list-ul"></i>
+
+                    <span>Ventes</span>
+
+                    <span class="pull-right-container">
+
+                        <i class="fa fa-angle-left pull-right"></i>
+
+                    </span>
+
+                </a>
+
+                <ul class="treeview-menu">
+
+                    <li>
+
+                        <a href="">
+
+                            <i class="fa fa-circle-o"></i>
+                            <span>Vente Administateur</span>
+                        </a>
+
+                    </li>
+
+                    <li>
+
+                        <a href="">
+
+                            <i class="fa fa-circle-o"></i>
+                            <span>Créer Vente</span>
+                        </a>
+
+                    </li>
+
+                    <li>
+
+                        <a href="">
+
+                            <i class="fa fa-circle-o"></i>
+                            <span>Rapport</span>
+                        </a>
+
+                    </li>
+
+                </ul>
+
+            </li>
+
+        </ul>
+
+    </section>
+
+</aside>
